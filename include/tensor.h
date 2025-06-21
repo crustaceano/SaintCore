bd494b4bd20a331327e8f2a4dfabcf36e09e12aa
MishaTuzov <misha.tuzov12@yandex.ru>
2025-06-21 19:17:41 +0300

    ZOV

--- include/tensor.h
+++ include/tensor.h
@@ -5,8 +5,20 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
-namespace SaintCore {
+#include <vector>
+#include "types.h"
 
+namespace SaintCore {
+	class Tensor {
+		int cols, rows;
+		std::vector<std::vector<floatT>> data;
+	public:
+		friend Tensor operator+(Tensor const& a, Tensor const& b);
+		friend Tensor operator*(Tensor const& a, Tensor const& b);
+		friend Tensor operator-(Tensor const& a, Tensor const& b);
+		friend Tensor operator*(Tensor const& a, int b);
+		friend Tensor operator%(Tensor const& a, Tensor const& b);
+	};
 }
 
 #endif //TENSOR_H
---
+++ include/types.h
@@ -0,0 +1,8 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+namespace SaintCore {
+	typedef floatT float;
+}
+
+#endif // TYPES_H
