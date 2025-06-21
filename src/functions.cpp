c472c46d94b7b89a5565a394fdf39b27ec889e8c
Omar Akhmedov <omar.axmedov.07@mail.ru>
2025-06-21 23:17:39 +0300

    bug fix functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,13 +1,13 @@
 #include <include/functions.h>
-#include <include/tensor.h>
+#include <cmath>
 
-SaintCore::Functions::Tensor SaintCore::Functions::softmax(const SaintCore::Functions::Tensor& input) {
-    SaintCore::Functions::Tensor output(input.get_rows(), input.get_cols());
+SaintCore::Tensor SaintCore::Functions::softmax(const SaintCore::Tensor& input) {
+    Tensor output(input.get_rows(), input.get_cols());
     float sum = 0.0f;
 
     for (int i = 0; i < input.get_rows(); ++i) {
         for (int j = 0; j < input.get_cols(); ++j) {
-            output[i][j] = exp(input[i][j]);
+            output[i][j] = std::exp(input[i][j]);
             sum += output[i][j];
         }
     }
