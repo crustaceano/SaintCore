18a0c3e6c49181e5acbf43753f1fe776209fd78b
Omar Akhmedov <omar.axmedov.07@mail.ru>
2025-06-22 13:46:37 +0300

    add other tests

--- tests/test_functions.cpp
+++ tests/test_functions.cpp
@@ -6,17 +6,19 @@
 
 TEST(test_exp_function, test_exp_working) {
     using namespace SaintCore;
-    EXPECT_EQ(Functions::exp(Tensor({{1, 2, 3}})),
-              Tensor({{static_cast<floatT>(std::exp(1)), static_cast<floatT>(std::exp(2)), static_cast<float>(std::exp(3))
+    EXPECT_EQ(Functions::exp(Tensor(std::vector<std::vector<float>>{{1, 2, 3}})),
+              Tensor({{static_cast<floatT>(std::exp(1)), static_cast<floatT>(std::exp(2)), static_cast<float>(std::exp(3
+                  ))
                   }}));
-    EXPECT_EQ(Functions::exp(Tensor({{1}, {2}, {3}})),
-              Tensor({{static_cast<floatT>(std::exp(1))}, {static_cast<floatT>(std::exp(2))}, {static_cast<float>(std::exp
+    EXPECT_EQ(Functions::exp(Tensor(std::vector<std::vector<float>>{{1}, {2}, {3}})),
+              Tensor({{static_cast<floatT>(std::exp(1))}, {static_cast<floatT>(std::exp(2))}, {static_cast<float>(std::
+                  exp
                   (3))}}));
 }
 
 TEST(test_sum, test_sum_dimensions) {
     using namespace SaintCore;
-    Tensor input({{1, 2, 3}, {4, 5, 6}});
+    Tensor input(std::vector<std::vector<float>>{{1, 2, 3}, {4, 5, 6}});
     Tensor row_sum = Functions::sum(input, -1);
     EXPECT_EQ(std::make_pair(row_sum.get_rows(), row_sum.get_cols()), std::make_pair(input.get_rows(), 1));
 
@@ -26,22 +28,22 @@ TEST(test_sum, test_sum_dimensions) {
 
 TEST(test_sum, test_sum_is_correct) {
     using namespace SaintCore;
-    Tensor input({{1, 2, 3, 4, 5}, {4, 5, 6, 7, 8}});
+    Tensor input(std::vector<std::vector<float>>{{1, 2, 3, 4, 5}, {4, 5, 6, 7, 8}});
     Tensor row_sum = Functions::sum(input, -1);
     EXPECT_EQ(std::make_pair(row_sum.get_rows(), row_sum.get_cols()), std::make_pair(2, 1));
-    EXPECT_EQ(row_sum, Tensor({
-        {15},
-        {30}
-    }));
+    EXPECT_EQ(row_sum, Tensor(std::vector<std::vector<float>>{
+                  {15.0},
+                  {30.0}
+                  }));
 
     Tensor col_sum = Functions::sum(input, 0);
     EXPECT_EQ(std::make_pair(col_sum.get_rows(), col_sum.get_cols()), std::make_pair(1, 5));
-    EXPECT_EQ(col_sum, Tensor({{5, 7, 9, 11, 13}}));
+    EXPECT_EQ(col_sum, Tensor(std::vector<std::vector<float>>{{5, 7, 9, 11, 13}}));
 }
 
 TEST(test_softmax, test_output_size) {
     using namespace SaintCore;
-    Tensor input({{1, 2, 3}, {4, 5, 6}});
+    Tensor input(std::vector<std::vector<float>>{{1, 2, 3}, {4, 5, 6}});
     Tensor output = Functions::softmax(input);
     EXPECT_EQ(std::make_pair(output.get_rows(), output.get_cols()),
               std::make_pair(input.get_rows(), input.get_cols()));
@@ -50,7 +52,7 @@ TEST(test_softmax, test_output_size) {
 
 TEST(test_softmax, testing_softmax_values) {
     using namespace SaintCore;
-    Tensor input({{1, 2, 3}, {4, 5, 6}});
+    Tensor input(std::vector<std::vector<float>>{{1, 2, 3}, {4, 5, 6}});
     Tensor output = Functions::softmax(input);
 
     for (int i = 0; i < output.get_rows(); ++i) {
