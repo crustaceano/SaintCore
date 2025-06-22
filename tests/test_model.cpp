5c655c24f08695fc2efa8ef42bf6a143182b917c
Omar Akhmedov <omar.axmedov.07@mail.ru>
2025-06-22 14:17:05 +0300

    add test_model

--- tests/test_model.cpp
+++ tests/test_model.cpp
@@ -7,12 +7,17 @@ TEST(test_model, test_linear_model_forward) {
     using namespace SaintCore::Models;
     using namespace SaintCore;
 
-    LinearModel model(3, 5);
+    LinearModel model(3, 2);
     Tensor input({{1.0f, 2.0f, 3.0f}, {4.0f, 5.0f, 6.0f}});
 
-    // Forward pass
+    std::cout << "Weights: \n" << model.get_weights() << '\n';
+    std::cout << "Bias: \n" << model.get_bias() << '\n';
+    std::cout << "input: \n" << input << '\n';
     Tensor output = model.forward(input);
 
+    std::cout << "output: \n" << output << '\n';
+
+
     EXPECT_EQ(output.get_rows(), input.get_rows());
-    EXPECT_EQ(output.get_cols(), 5);
+    EXPECT_EQ(output.get_cols(), 2);
 }
